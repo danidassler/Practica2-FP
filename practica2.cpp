4ef238de4fc585204af45b38f0f02e3cfb5462c5
Daniel Sanz Mayo <34971321+danidassler@users.noreply.github.com>
2018-01-03 14:46:07 +0100

    Update practica2.cpp

--- practica2.cpp
+++ practica2.cpp
@@ -12,80 +12,78 @@
 using namespace std;
 typedef enum { Rojo, Azul, Verde, Amarillo, Marron, Blanco, Incorrecto } tColor; //tipo enumerado de los posibles colores que puede usar el programa
 const int tam =4;
-typedef int tCodigo[tam];
+typedef tColor tCodigo[tam];
 int menu();
 char color2char(tColor color);
-int char2color(string cadena);
+void mostrarEleccion(tColor colorM,tCodigo codigoM, char letraM);
+tColor char2color(string cadena, int i);
 void codigoAletorio(tCodigo codigoM, bool admiteRepetido);
 void compararCodigos(const tCodigo codigoM, const tCodigo hipotesis, int& colocados, int& descolocados);
-void introducirHipotesis();
+void introducirHipotesis(tCodigo hipotesis);
 void mostrarDescripcion();// funcion para mostrar la descripcion del juego
 
 int main(){
-	tColor color1;
-	int intentos=1;
-	int longitud;
-	int colocados=0, descolocados=0;
+	int intentos=1, longitud, colocados=0, descolocados=0;
 	tColor colorM, colorH;
 	tCodigo codigoM, hipotesis;
 	srand(time(NULL));
 	tCodigo codigo;
 	char codigoH[tam];
-	bool admiteRepetido;
-	char letraM, letraH, letra1;
-	int op, num;
+	bool admiteRepetido, winner=false;
+	char letraM, letraH;
 	string cadena;
-	cout<<"numero del color que elijes:";//prueba para ver si convierte el color en una letra
-	cin>>op;
-	color1=tColor(op-1);
-	letra1=color2char(color1);
-	cout<<letra1<<endl;
-	// prueba prueba prueba prueba prueba prueba prueba (a partir de aqui bien)
     mostrarDescripcion();
 	int opcion=menu();
 	while(opcion!=0){
-			switch(opcion){
-				case 1: {
-					admiteRepetido=false;
-					codigoAletorio(codigoM, admiteRepetido);
-					for(int i=0;i<tam;i++){ // mostrar la eleccion del ordenador para depurar
-					 cout<<codigoM[i]+1;
-					 colorM=tColor(codigoM[i]);
-					 letraM=color2char(colorM);
-					 cout<<" --> "<<letraM<<endl;	
-					}
-					while(intentos<3){
-					 introducirHipotesis();
-						intentos++;//mirar tener cuidado
-					}
-					
+		switch(opcion){
+			case 1: {
+				admiteRepetido=false;
+				codigoAletorio(codigoM, admiteRepetido);
+			    mostrarEleccion(colorM, codigoM ,letraM);
+				while((intentos!=16)&&(winner==false)){
+				 introducirHipotesis(hipotesis);
+				 compararCodigos(codigoM, hipotesis, colocados, descolocados);
+				  if (colocados==4){
+					  winner=true;
+				     cout<< " Enhorabuena! Lo encontraste!"<<endl;
+				     cout<<" Te ha costado "<<intentos<<"intento(s)"<<endl;
+				    }
+					intentos++;
+					colocados=0;
+					descolocados=0;//reiniciar para que no se sumen 
 				}
-				break;
-				case 2: {
-					admiteRepetido=true;
-					codigoAletorio(codigoM, admiteRepetido);
-					for(int i=0;i<tam;i++){ // mostrar la eleccion del ordenador para depurar
-					 cout<<codigoM[i]+1;
-					 colorM=tColor(codigoM[i]);
-					 letraM=color2char(colorM);
-					 cout<<" --> "<<letraM<<endl;	
-					}
-					while(intentos<15){
-						
-						}//pene
-						intentos++;//mirar tener cuidado
-					}
-					
-				}
-				break;
+				if(intentos==16){
+				 cout<<" Has perdido , no has conseguido adivinar el codigo secreto !"<<endl;
+			    }
 			}
-		
-		 if(intentos==15){
-			cout<< " Has agotado tus intentos "<<endl;
+			break;
+			case 2: {
+				admiteRepetido=true;
+				codigoAletorio(codigoM, admiteRepetido);
+				mostrarEleccion(colorM, codigoM ,letraM);
+				while((intentos!=16)&&(winner==false)){
+				 introducirHipotesis(hipotesis);
+				 compararCodigos(codigoM, hipotesis, colocados, descolocados);
+				  if (colocados==4){
+					 winner=true;
+				     cout<< " Enhorabuena! Lo encontraste!"<<endl;
+				     cout<<" Te ha costado "<<intentos<<"intento(s)"<<endl;
+				    }
+					intentos++;
+					colocados=0;
+					descolocados=0;//reiniciar para que no se sumen 
+				}
+				if(intentos==16){
+					cout<<" Has perdido , no has conseguido adivinar el codigo secreto !"<<endl;
+				}
+			}
+			break;
+		}
+		if(intentos==16){
+		 cout<<" Has perdido , no has conseguido adivinar el codigo secreto !"<<endl;
 		}
 		opcion=menu();
 	}
-
 	return 0;
 }
 
@@ -131,44 +129,41 @@ char color2char(tColor color){ // funcion que convierte un entero relacionado co
 	}
 	return caracter;
 }
-int char2color(string cadena){
-	tColor color;
-	
-	for(int k=0;k<tam;k++){
-		cadena[k]=toupper(cadena[k]);//para que se pueda usar tanto minusculas como mayusculas
-	
-	 switch(cadena[k]){
-			  case 'R':{
-			 color=Rojo;
-			}
-			break;
-		 case 'Z': {
-			 color=Azul; 
-			}
-			break;
-		 case 'V': {
-			 color=Verde; 
-			}
-			break;
-		 case 'A': {
-			 color=Amarillo; 
-			}
-			break;
-		 case 'M': {
-			 color=Marron; 
-			}
-			break;
-		 case 'B': {
-			 color=Blanco;
-			}
-			break;
-		 default: {
-			 color=Incorrecto;
-		    }
+
+tColor char2color(string cadena, int i){
+ tColor color;
+ cadena[i]=toupper(cadena[i]);//para que se pueda usar tanto minusculas como mayusculas
+
+ switch(cadena[i]){
+	 case 'R':{
+		 color=Rojo;
+		 break;
 	    }
+	 case 'Z': {
+		 color=Azul; 
+		 break;
+	    }
+	 case 'V': {
+		 color=Verde; 
+		 break;
+		}
+	 case 'A': {
+		 color=Amarillo; 
+		 break;
+	    }
+	 case 'M': {
+		 color=Marron; 
+		 break;
+		}
+	 case 'B': {
+		 color=Blanco;
+		 break;
+		}
+	 default: {
+		 color=Incorrecto;
+		}
 	}
-	
-	return color;
+return color;
 }
 
 void mostrarDescripcion(){
@@ -193,20 +188,20 @@ void codigoAletorio(tCodigo codigoM, bool admiteRepetido){
 	if(admiteRepetido==true){
 		for(int i=0;i<tam;i++){
 		  eleccion=rand()%(6);
-		  codigoM[i]=eleccion;
+		  codigoM[i]=tColor(eleccion);
 		}
 	}
 	else{
 		for(int p=0;p<tam;p++){
 		 eleccion=(rand()%(6));
-		 codigoM[p]=eleccion;
+		 codigoM[p]=tColor(eleccion);
 		}
 		for(int a=0;a<tam;a++){
 			for(int b=0;b<tam;b++){
 				if(a!=b){
 					while(codigoM[a]==codigoM[b]){
 						eleccion=rand()%(6);
-						codigoM[b]= eleccion;
+						codigoM[b]=tColor(eleccion);
 					}
 				}
 			}
@@ -226,7 +221,7 @@ void codigoAletorio(tCodigo codigoM, bool admiteRepetido){
 				   if(c!=d){
 				      while(codigoM[c]==codigoM[d]){
 				          eleccion=rand()%(6);
-					     codigoM[d]= eleccion;
+					     codigoM[d]=tColor(eleccion);
 					    }
 				    }
 			    }
@@ -236,42 +231,69 @@ void codigoAletorio(tCodigo codigoM, bool admiteRepetido){
     }
 }
 
-void introducirHipotesis(){
+void introducirHipotesis(tCodigo hipotesis){
 	string cadena;
-	tCodigo hipotesis;
 	int longitud;
 	cout<<" Introduce el codigo (palabra de 4 letras con alguna de R(1), Z(2), V(3), A(4), M(5) o B(6): "<<endl;
 	cout<<" Codigo : ";
 	cin>>cadena;
-	for(int i=0;i<tam;i++){
-	 hipotesis[i]=char2color(cadena);
-	}
 	longitud=cadena.length();
 	while(longitud!=4){
 		cout<<" Codigo incorrecto, vuelve a introducir un codigo. "<<endl;
 		cout<<" Codigo : ";
 		cin>>cadena;
-		for(int i=0;i<tam;i++){
-		 hipotesis[i]=char2color(cadena);
-		}
+		longitud=cadena.length();
+	}
+	for(int i=0;i<tam;i++){
+	 hipotesis[i]=char2color(cadena, i);
+	 while((hipotesis[i]==Incorrecto)||(longitud!=4)){
+		 cout<<" Codigo incorrecto, vuelve a introducir un codigo. "<<endl;
+		 cout<<" Codigo : ";
+		 cin>>cadena;
+		 longitud=cadena.length();
+		 hipotesis[i]=char2color(cadena, i);
+	    }
 	}
 }
 	
 void compararCodigos(const tCodigo codigoM, const tCodigo hipotesis, int& colocados, int& descolocados){
-    for(int z=0;z<tam;z++){
-		if(codigoM[z]==hipotesis[z]){
+	typedef bool tComp[4];//VAMR
+	tComp comparacionH;
+	tComp comparacionM;
+	for(int i=0;i<tam;i++){
+		comparacionM[i]=false;
+		comparacionH[i]=false;
+	}
+  	for(int z=0;z<tam;z++){//VAMR y RRRR
+		if(codigoM[z]==hipotesis[z]){// VyR  AyR MyR RyR
 			colocados++;
+			comparacionM[z]=true;
 		}
 	}
-	if(colocados==4){
-		
-		
+	if(colocados!=tam){
+       for(int j=0;j<tam;j++){
+		  if(comparacionM[j]==false){
+		     for(int e=0;e<tam;e++){
+			     if((j!=e)&&(comparacionH[e]==false)&&(comparacionM[j]==false)){
+				     if(codigoM[j]==hipotesis[e]){
+					     descolocados++;
+					     comparacionH[e]=true;
+					     comparacionM[j]=true;
+					    }
+				    }
+				}
+			}
+		}
 	
-	
-	
-	
-	
-	
-	
+	}		
+    cout<<" Colocados: "<<colocados<<"; Descolocados: "<<descolocados<<endl; 
 }
-	
+
+void mostrarEleccion(tColor colorM,tCodigo codigoM, char letraM){
+	for(int i=0;i<tam;i++){ // mostrar la eleccion del ordenador para depurar
+	 cout<<codigoM[i]+1;
+	 colorM=tColor(codigoM[i]);
+	 letraM=color2char(colorM);
+	 cout<<" --> "<<letraM<<endl;
+	}
+}
